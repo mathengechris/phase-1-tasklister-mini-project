d62d446c46e0608b28334edb1cae7cd7bbad8211
mathengechris <mathengechris27@gmail.com>
2026-01-03 00:37:57 -0800

    initial

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+
+  function buildToDo(task) {
+    const li = document.createElement("li");
+    li.textContent = task;
+    const tasklist = document.getElementById("tasks")
+  }
+  const form = document.getElementById("create-task-form");
+
+  form.addEventListener("submit", (event) => {
+    event.preventDefault()
+
+    const taskInput = event.target["new-task-description"] . value;
+
+    buildToDo(taskInput);
+
+    event.target["new-task-description"].value = "";
+
+  });
 });
